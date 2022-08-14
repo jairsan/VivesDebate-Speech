18011d39aff1db58ef3460dd27411ad161f01d2d
Javier Iranzo <jairsan@upv.es>
2022-08-14 11:43:34 +0200

    Fix align, add timestamps

--- data_preparation/add_timestamps_to_BIOS.py
+++ data_preparation/add_timestamps_to_BIOS.py
@@ -2,6 +2,7 @@ import json
 import os
 from itertools import zip_longest
 
+
 def write_timestamps(bio_file_fn, align_file_fn, output_file_fn):
     with open(bio_file_fn) as bio_file, open(align_file_fn) as align_file, open(output_file_fn,"w") as output_file:
         data = json.load(align_file)
@@ -9,20 +10,25 @@ def write_timestamps(bio_file_fn, align_file_fn, output_file_fn):
         for segment in data:
             word_groups = segment['wl']
             for word_group in word_groups:
-                for word in word_group:
-                    out.append((word['b'], word['e'], word['w']))
+                for token in word_group["w"].split():
+                    out.append((str(word_group['b']), str(word_group['e']), token))
 
         for bio_line, out in zip_longest(bio_file, out):
-            bio_line = bio_line.strip()
+            if bio_line is not None:
+                bio_line = bio_line.strip().split()
+                word = bio_line[0]
+                label = bio_line[1]
+            else:
+                word, label = "None", "None"
             out_line = " ".join(out)
-            print(bio_line+out_line, file=output_file)
+            print(word + " " + out_line + " " + label, file=output_file)
 
 
 if __name__ == "__main__":
     """
     Run from this folder
     """
-    os.makedirs("DATA/BIO_timestamps/")
+    os.makedirs("DATA/BIO_timestamps/",exist_ok=True)
 
     for i in range(1, 30):
         bio_file = "DATA/BIO/Debate" + str(i) + ".txt"
