b5d836b9e09378d39bc89554a873274f45d0a2eb
Javier Iranzo <jairsan@upv.es>
2022-09-01 12:11:59 +0200

    ...

--- src/train/train_transformers_classifier.py
+++ src/train/train_transformers_classifier.py
@@ -1,4 +1,13 @@
-from train_segment_classifier import SegmentClassifier
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
+
 
 if __name__ == "__main__":
-    clas = SegmentClassifier()
+    model_name = "softcatala/fullstop-catalan-punctuation-prediction"
+
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    pipeline = TextClassificationPipeline(model=model, tokenizer=tokenizer)
+
+    pipeline("Els investigadors suggereixen que tot i que es tracta de la cua d'un dinosaure jove la mostra revela un plomatge adult i no pas plomissol")
