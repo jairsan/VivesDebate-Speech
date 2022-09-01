90405ab33019bcc190f30793739501e5d85c8d75
Javier Iranzo <jairsan@upv.es>
2022-09-01 12:28:59 +0200

    ...

--- src/train/train_transformers_classifier.py
+++ src/train/train_transformers_classifier.py
@@ -1,13 +1,13 @@
 
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
+from transformers import AutoTokenizer, AutoModelForTokenClassification, TokenClassificationPipeline
 
 
 if __name__ == "__main__":
     model_name = "softcatala/fullstop-catalan-punctuation-prediction"
 
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    model = AutoModelForTokenClassification.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    pipeline = TextClassificationPipeline(model=model, tokenizer=tokenizer)
+    pipeline = TokenClassificationPipeline(model=model, tokenizer=tokenizer)
 
-    pipeline("Els investigadors suggereixen que tot i que es tracta de la cua d'un dinosaure jove la mostra revela un plomatge adult i no pas plomissol")
+    print(pipeline("Els investigadors suggereixen que tot i que es tracta de la cua d'un dinosaure jove la mostra revela un plomatge adult i no pas plomissol", aggregation_strategy="first"))
