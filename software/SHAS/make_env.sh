5707f3b1001839196f6e8b49a46050e5dc6327b2
Javier Iranzo <jairsan@upv.es>
2022-09-16 16:07:01 +0200

    Add submodule

--- software/SHAS/make_env.sh
+++
@@ -1,9 +0,0 @@
-conda env create -f environment.yml
-
-# To activate this environment, use
-#
-#     $ conda activate shas
-#
-# To deactivate an active environment, use
-#
-#     $ conda deactivate
--- software/SHAS/src/__init__.py
+++
@@ -1,3 +0,0 @@
-import os, sys
-print(os.path.dirname(__file__))
-sys.path.append(os.path.dirname(__file__))
