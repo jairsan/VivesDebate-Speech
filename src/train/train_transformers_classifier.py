ce9b80c68bebdb31498f46785f7bfe70a040712f
Javier Iranzo <jairsan@upv.es>
2022-09-17 18:11:30 +0200

    Save best model

--- src/train/train_transformers_classifier.py
+++ src/train/train_transformers_classifier.py
@@ -335,9 +335,14 @@ def train_model(model_name: str, train_files: List[str], eval_files: List[str], 
     def compute_metrics(eval_preds):
         f1 = evaluate.load("f1")
         acc = evaluate.load("accuracy")
+        #metrics = evaluate.combine(["f1", "accuracy"])
         logits, labels = eval_preds
         predictions = np.argmax(logits, axis=-1)
-        return {"f1": f1.compute(predictions=predictions, references=labels), "acc": acc.compute(predictions=predictions, references=labels)}
+
+        # Compute_metrics has to return a non nested Dict[str, Any]
+        return {"f1": f1.compute(predictions=predictions, references=labels)["f1"],
+                "accuract": acc.compute(predictions=predictions, references=labels)["accuracy"]}
+        # return metrics.compute(predictions=predictions, references=labels)
 
     hf_trainer_training_args = TrainingArguments(
         output_dir=output_dir_name + "_models",  # output directory
